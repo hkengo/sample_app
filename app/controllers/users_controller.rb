92873a60d011d98c705fc36e206dd114887331bb
hayashi_kengo <hayashikengojp@gmeil.com>
2016-03-11 18:11:35 +0900

    Add Users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+  def show
+    @title = "Show"
+    @user = User.find(params[:id])
+    #debugger       デバッグに有効
+  end
+
   def new
     @title = "Sign up"
   end
