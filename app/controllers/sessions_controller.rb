f0f922dcccb818785094bcdfd46e2d39350a63e9
hayashi_kengo <hayashikengojp@gmeil.com>
2016-03-18 20:05:55 +0900

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Success login."
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user
@@ -19,6 +20,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to root_url, notice: '----- success logout -----'
+    flash[:success] = "Success log out."
+    redirect_to root_url
   end
 end
