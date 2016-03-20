8ac8be76550f9f064e8ae8f4e71673a94eb5cc06
hayashi_kengo <hayashikengojp@gmeil.com>
2016-03-21 00:57:42 +0900

    setting production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,10 +62,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '【ここにHerokuのサブドメインを入力】.herokuapp.com'
+  host = 'sample-app-sns.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
